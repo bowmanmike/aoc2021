6854c35d8dfd45a899c03ecf3f91cdba7b1335fd
Mike Bowman <mbowman2@gmail.com>
2021-12-03 14:59:19 -0500

    Finish day3

--- lib/day03.ex
+++ lib/day03.ex
@@ -11,6 +11,47 @@ defmodule Aoc2021.Day03 do
     |> sum_result()
   end
 
+  def part_two(input) do
+    zipped_input = input |> Enum.map(&String.split(&1, "", trim: true)) |> Enum.zip()
+    o2_rating = filter(input, zipped_input, 0, &max/1)
+    co2_rating = filter(input, zipped_input, 0, &min/1)
+
+    o2_rating * co2_rating
+  end
+
+  defp max(enum) do
+    Enum.max_by(enum, fn {k, v} -> {v, k} end)
+  end
+
+  defp min(enum) do
+    Enum.min_by(enum, fn {k, v} -> {v, k} end)
+  end
+
+  defp filter([input | []], _zipped, _count, _compare_func) do
+    input |> String.split("", trim: true) |> decimal_from_binary_int()
+  end
+
+  defp filter(input, zipped, count, compare_func) do
+    {starting_digit, _} =
+      zipped
+      |> List.first()
+      |> Tuple.to_list()
+      |> Enum.frequencies()
+      |> compare_func.()
+
+    remaining =
+      input
+      |> Enum.filter(fn elem -> String.at(elem, count) == starting_digit end)
+
+    remaining_zipped =
+      remaining
+      |> Enum.map(&String.split(&1, "", trim: true))
+      |> Enum.zip()
+      |> Enum.drop(count + 1)
+
+    filter(remaining, remaining_zipped, count + 1, compare_func)
+  end
+
   defp calc(%Result{input: input} = data) do
     %{gamma: raw_gamma, epsilon: raw_epsilon} =
       input
@@ -23,11 +64,15 @@ defmodule Aoc2021.Day03 do
         %{acc | gamma: acc.gamma ++ [max], epsilon: acc.epsilon ++ [min]}
       end)
 
-    gamma = raw_gamma |> Enum.map(&String.to_integer/1) |> Integer.undigits(2)
-    epsilon = raw_epsilon |> Enum.map(&String.to_integer/1) |> Integer.undigits(2)
+    gamma = decimal_from_binary_int(raw_gamma)
+    epsilon = decimal_from_binary_int(raw_epsilon)
 
     %Result{data | gamma: gamma, epsilon: epsilon}
   end
 
   defp sum_result(%Result{gamma: gamma, epsilon: epsilon}), do: gamma * epsilon
+
+  defp decimal_from_binary_int(num) do
+    num |> Enum.map(&String.to_integer/1) |> Integer.undigits(2)
+  end
 end
--- test/day03_test.exs
+++ test/day03_test.exs
@@ -29,4 +29,31 @@ defmodule Aoc2021.Day03Test do
       assert Day03.part_one(input) == 749_376
     end
   end
+
+  describe "part two" do
+    test "sample input" do
+      input = [
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+      ]
+
+      assert Day03.part_two(input) == 230
+    end
+
+    test "unique input" do
+      input = Utils.read_file("inputs/day03.txt")
+
+      assert Day03.part_two(input) == 2_372_923
+    end
+  end
 end
