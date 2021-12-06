3a673020aae3cc5f78ef3b62c178dfbc7d5d0e65
Mike Bowman <mbowman2@gmail.com>
2021-12-05 19:38:38 -0500

    Finish setup

--- lib/day05.ex
+++ lib/day05.ex
@@ -3,13 +3,38 @@ defmodule Aoc2021.Day05 do
     defstruct [:start, :finish]
   end
 
+  defmodule Point do
+    defstruct [:x, :y]
+  end
+
   def part_one(input) do
+    parsed_input =
+      setup(input)
+      |> Enum.filter(fn
+        %Entry{start: %Point{x: x}, finish: %Point{x: x}} -> true
+        %Entry{start: %Point{y: y}, finish: %Point{y: y}} -> true
+        _ -> false
+      end)
+
+    require IEx; IEx.pry()
+
+    parsed_input
+  end
+
+  defp setup(input) do
     input
     |> String.split("\n", trim: true)
     |> Enum.map(fn elem ->
-      [start, finish] = String.split(elem, " -> ")
+      [start, finish] =
+        elem
+        |> String.split(" -> ")
+        |> IO.inspect()
+        |> Enum.map(fn point ->
+          [x, y] = String.split(point, ",", trim: true)
+          %Point{x: String.to_integer(x), y: String.to_integer(y)}
+        end)
 
-      %Entry{start: String.to_float(start), finish: String.to_float(finish)}
+      %Entry{start: start, finish: finish}
     end)
   end
 end
